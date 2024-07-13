af472a519509c52c2becc828c6e87d8d3d71a8db
YourAKShaw <hitme@yourakshaw.com>
2024-07-14 01:30:37 +0530

    feat(env): :sparkles: incorporate dotenv

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
+
 import printAsciiArt from './common/helpers/printAsciiArt.js';
 import app from './app.js';
 
 printAsciiArt();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
