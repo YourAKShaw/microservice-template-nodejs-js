977a5fb337d87ef40cee583e0afb30710b7a6c9f
YourAKShaw <hitme@yourakshaw.com>
2024-07-14 20:42:49 +0530

    feat(api): :sparkles: implement custom ApiResponse class for responses

---
+++ src/common/apiResponse.js
@@ -0,0 +1,29 @@
+/**
+ * Represents an API response.
+ * @typedef {Object} apiResponse
+ */
+class apiResponse {
+  statusCode;
+  success;
+  message;
+  data;
+  errors;
+
+  /**
+   * @constructor
+   * @param {number} statusCode
+   * @param {boolean} success
+   * @param {string} message
+   * @param {any} data
+   * @param {any[]} errors
+   */
+  constructor({ statusCode, success, message, data, errors }) {
+    this.statusCode = statusCode || 500;
+    this.success = success || false;
+    this.message = message || '';
+    this.data = data || null;
+    this.errors = errors || [];
+  }
+}
+
+export default apiResponse;
