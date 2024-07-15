97b7ac688abe246bafd691c66022082be639d24b
YourAKShaw <hitme@yourakshaw.com>
2024-07-15 15:43:55 +0530

    feat(sample): :card_file_box: add create-sample.schema.js

---
+++ src/sample/schema/create-sample.schema.js
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+const createSampleSchema = Joi.object({
+  sampleBoolean: Joi.boolean(),
+  sampleNumber: Joi.number(),
+  sampleString: Joi.string()
+}).required();
+
+export default createSampleSchema;
